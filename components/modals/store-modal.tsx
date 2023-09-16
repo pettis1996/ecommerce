50640b76204922297466f35de711c9d3de5486a5
pettis1996 <cs03136@uoi.gr>
2023-09-16 15:31:20 +0300

    Dashboard update

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -43,6 +43,8 @@ export const StoreModal = () => {
             const response = await axios.post('/api/stores', values);
 
             toast.success("Store Created Successfully!");
+
+            window.location.assign(`/${response.data.id}`);
         } catch (error) {
             toast.error("Something went wrong...");
         } finally {
